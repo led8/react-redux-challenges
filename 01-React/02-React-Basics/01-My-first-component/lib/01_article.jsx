ccf1150de9301cfbd7a600d56989ffc263e96545
Arthur <a.dhuy@orange.fr>
2019-05-02 00:08:07 +0200

    My-first-component done

--- 01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
+++ 01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
@@ -1,9 +1,17 @@
-// TODO: import the right dependencies
+import React from 'react';
+// import ReactDOM from 'react-dom';
 
-// TODO: code the Article component
+const Article = (props) => {
+  return (
+    <div className='article'>
+      <h2>{props.title}</h2>
+      <p>{props.body}</p>
+    </div>
+  );
+};
 
+// const container = document.querySelector('.container');
+// ReactDOM.render(<Article title="My fancy new product" body="Test it now!" />, container);
 
+export default Article;
 
-
-
-// TODO: export your Article component for tests
