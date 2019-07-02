b15ade97926fd40a0cedb1ec29b2e9570e800bf2
Arthur <a.dhuy@orange.fr>
2019-07-02 16:50:01 +0200

    Fix it

--- 01-React/02-React-Basics/02-Adding-state/lib/01_article_with_state.jsx
+++ 01-React/02-React-Basics/02-Adding-state/lib/01_article_with_state.jsx
@@ -1,17 +1,37 @@
-// TODO: update dependencies if needed
-import React from 'react';
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
-// TODO: transform the following functional component into a class based component
-// TODO: set the initial state { clicked: false }
-// TODO: listen to the click event on `.article`
-// TODO: code the `handleClick` function (which adds a 'clicked' class to the `.article`)
-const Article = (props) => {
-  return (
-    <div className="article">
-      <h2 className="article-title">{props.title}</h2>
-      <p>{props.body}</p>
-    </div>
-  );
+import './assets/stylesheets/application.scss';
+
+class Article extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    console.log("hello");
+    this.setState({
+      clicked: true
+    });
+  }
+
+  render() {
+    return (
+      <div className={this.state.clicked ? "clicked" : "article"} onClick = {this.handleClick}>
+        <h2>{this.props.title}</h2>
+        <p>{this.props.body}</p>
+      </div>
+    );
+  }
 }
+
+const container = document.querySelector('.container');
+ReactDOM.render(<Article title="My fancy new product" body="Test it now!" />, container);
 
 export default Article;
