75db0339ebbc23b66afd8da7c1012002a10e4e0c
Arthur <a.dhuy@orange.fr>
2019-07-02 13:17:34 +0200

    fix first component

--- 01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
+++ 01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
+// import React, { Component } from 'react';
 
 const Article = (props) => {
   return (
@@ -10,8 +11,35 @@ const Article = (props) => {
   );
 };
 
-// const container = document.querySelector('.container');
-// ReactDOM.render(<Article title="My fancy new product" body="Test it now!" />, container);
+// class Article extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       counter: 0
+//     };
+
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState({
+//       counter: this.state.counter + 1
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className='article' onClick = {this.handleClick}>
+//         <h2>{this.props.title}</h2>
+//         <p>{this.props.body}</p>
+//         <p>{this.state.counter}</p>
+//       </div>
+//     );
+//   }
+// }
+
+const container = document.querySelector('.container');
+ReactDOM.render(<Article title="My fancy new product" body="Test it now!" />, container);
 
 export default Article;
-
